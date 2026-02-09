5ac2311a8da93761509715d1cddaf51580a82135
adizelkin-dotcom <adi.zelkin@gmail.com>
2026-02-09 19:37:45 +0200

    i added the extra things that i need for this code and chenged what i neede

--- data/code.py
+++ data/code.py
@@ -1,5 +1,5 @@
 file=open("data\holder.gitkeep")
-have=input("Does the Female has a BRCA1,2 mutation? (Y=Yes, N=No) N")
+have=input("Does the Female has a BRCA1,2 mutation? (Y=Yes, N=No) ")
 
 import random
 def Mutate_DNA(seq):
@@ -58,3 +58,37 @@ def Comp_seq(old,new):
       counter+=1
   return counter
 
+seq=""
+
+for line in file:
+   seq+=DNA_RNA_Cod(line)
+   mut=seq
+count=0
+sum=0
+y=0
+n=0
+for i in range(1000):
+ time=0
+ while mut== seq:
+   time+=1
+   typ=random.randint(1,3)
+   if typ==1:
+     seq=Mutate_DNA(seq)
+   elif typ==2:
+     Delete_DNA(seq)
+   elif typ==3:
+      Insert_DNA(seq)
+ sum+=time
+ count+=1
+ if have=="N" and count>=2:
+   n+=1
+ if have=="Y" and count>=1:
+   y+=1
+
+years=sum*24
+
+if have=="Y":
+  print("For a female that does have BRCA1,2 Mutation: The mutation that will change the P53 protein will take in average",round(y/years,4),"years.")
+
+else:
+  print("For a female that does not have BRCA1,2 Mutation: The mutation that will change the P53 protein will take in average",round(n/years,4),"years.")
